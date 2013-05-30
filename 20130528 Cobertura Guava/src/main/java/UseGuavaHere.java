a6bb3204f594304fba2e03539572ae12678c7307
cuipengfei <cuipengfei2008@gmail.com>
2013-05-31 01:07:34 +0800

    重现出来了

--- 20130528 Cobertura Guava/src/main/java/UseGuavaHere.java
+++ 20130528 Cobertura Guava/src/main/java/UseGuavaHere.java
@@ -4,17 +4,7 @@ import com.google.common.collect.Lists;
 import java.util.ArrayList;
 import java.util.List;
 
-public class UseGuavaHere extends ParentClass {
-    public List<String> tryUseGuava() {
-        ArrayList<String> integers = Lists.newArrayList("1", "2", "3");
-        return Lists.transform(integers, new Function<String, String>() {
-            @Override
-            public String apply(String str) {
-                return makeString(str);
-            }
-        });
-    }
-
+public class UseGuavaHere {
     public List<String> tryUseGuava2() {
         ArrayList<String> integers = Lists.newArrayList("1", "2", "3");
         return Lists.transform(integers, new Function<String, String>() {
--- 20130528 Cobertura Guava/src/test/java/UseGuavaHereTest.java
+++ 20130528 Cobertura Guava/src/test/java/UseGuavaHereTest.java
@@ -2,23 +2,13 @@ import org.junit.Test;
 
 import java.util.List;
 
-import static org.hamcrest.core.Is.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
 public class UseGuavaHereTest {
     @Test
-    public void testTryUseGuava() throws Exception {
-        List<String> strings = new UseGuavaHere().tryUseGuava();
-        assertThat(strings.get(0), is("1hello"));
-        assertThat(strings.get(1), is("2hello"));
-        assertThat(strings.get(2), is("3hello"));
-    }
-
-    @Test
     public void testTryUseGuava2() throws Exception {
         List<String> strings = new UseGuavaHere().tryUseGuava2();
-        assertThat(strings.get(0), is("1hello"));
-        assertThat(strings.get(1), is("2hello"));
-        assertThat(strings.get(2), is("3hello"));
+        assertThat(strings, notNullValue());
     }
 }
