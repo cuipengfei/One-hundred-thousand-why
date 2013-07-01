e5e0b4f032d4f72ba5c78eaea09018b502616e88
cuipengfei <cuipengfei2008@gmail.com>
2013-07-01 21:21:32 +0800

    set up webapp

---
+++ 20130701SessionAttributesLifeCycle/SpringMVC/src/main/java/com/mkyong/common/controller/FirstController.java
@@ -0,0 +1,14 @@
+package com.mkyong.common.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@Controller
+public class FirstController {
+    @RequestMapping(value = "firstpage", method = GET)
+    public String myHander() {
+        return "firstpage";
+    }
+}
